ee41d338018d3a1a2505ad980f9149c6f3fd99a0
Batunash <okullubatuhan@gmail.com>
2026-07-24 17:23:53 +0300

    chore: bump versions for pypi and vsce release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nimcode",
-    version="0.1.12",
+    version="0.1.13",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
